c8a3d013ad06a64bec68133369a53f291f06cad1
Belén Albeza <ladybenko@gmail.com>
2009-05-07 19:52:58 +0200

    Rdoc'd MenuState

--- engine/menu_state.rb
+++ engine/menu_state.rb
@@ -6,6 +6,7 @@ module Engine
     TextY = (2 * Game::ScreenHeight) / 3
     TextGap = 50
     
+    # Constructor
     def initialize
       @img_background = Game::images["background"]
       @img_logo = Game::images["logo"]
@@ -15,10 +16,11 @@ module Engine
       @selected = 0
     end
     
+    # Updates the main menu screen elements
     def update
-      
     end
     
+    # Draws the main menu screen elements
     def draw
       @img_background.draw(0, 0, 0)
       @img_logo.draw_rot(Game::ScreenWidth / 2, 120, ZOrder::Hud, 0)
@@ -29,12 +31,14 @@ module Engine
       end
     end
     
+    # Gets called automatically when the user presses down a button
     def button_down(id)
       case id
       when Gosu::KbEscape then Game.quit
       end
     end
     
+    # Gets called automatically when the user releases a button
     def button_up(id)
       case id
       when Gosu::KbDown then next_option!
@@ -43,18 +47,22 @@ module Engine
       end
     end
     
+    # Checks whether the given menu option is tagged as selected or not
     def option_selected?(i)
       @selected == i
     end
     
+    # Tags the next menu option as selected
     def next_option!
       @selected = (@selected + 1) % @options.size
     end
     
+    # Tags the previous menu option as selected
     def prev_option!
       @selected = (@selected - 1) % @options.size
     end
     
+    # Executes the current highlighted option
     def select_option
       case @options[@selected]
       when "New Game" then Game.game_state = PlayState
